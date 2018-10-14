82e74cdfbafaf1c03caa13691c81808ef74c3b03
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-10-14 17:21:00 +0200

    Add route params validation

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,30 @@ import { stream } from './logger'
 const app = express()
 app.use(morgan('combined', { stream }))
 
-app.get('/convert', convertRoute)
+const validationRules = [
+  check('amount')
+    .not().isEmpty()
+    .isFloat(),
+  check(['src_currency', 'dest_currency'])
+    .not().isEmpty()
+    .isString()
+    .isUppercase()
+    .isLength({ min: 3, max: 3 }),
+  check('reference_date')
+    .optional()
+    .isString()
+    .matches(/\d{4}-\d{2}-\d{2}/),
+]
+
+app.get('/convert', validationRules, convertRoute)
 
 function convertRoute(req: Request, res: Response) {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    res.status(400).send({ errors: errors.array(), status: 400 })
+    return
+  }
+
   const amount = req.query.amount
   const sourceCurrency = req.query.src_currency
   const destCurrency = req.query.dest_currency
